214263cf695207f6ee7ca6f08c421cc07555659d
Christopher Lindsay <christopher.m.lindsay@gmail.com>
2024-05-03 23:08:44 -0400

    feat: added azlogin function with prompt function

--- LIB/functions.zsh
+++ LIB/functions.zsh
@@ -10,6 +10,9 @@ function install_functions() {
     target_dir=$functions_dir/$1
     functions_list=($(ls functions))
 
+    echo "Uninstalling previously existing functions..."
+    unfunction -m '.*'
+
     echo Installing: ${functions_list}
     mkdir -p $target_dir
     cp -r functions/* $target_dir
@@ -42,5 +45,38 @@ EOF
         echo
     fi
 
-    echo "Functions have been installed and will be available in a new shell."
+    echo "Functions have been installed and are now active in this shell."
+}
+
+
+# To add subscription to prompt... 
+function add_azure_prompt_to_zshrc() {
+    ZSHRC="$HOME/.zshrc"
+
+    # Function and prompt to add
+    function_text="function get_current_azure_subscription() {
+        local sub_name=\$(az account show --query name -o tsv 2>/dev/null)
+        if [[ -n \"\$sub_name\" ]]; then
+            echo \"%{\$fg[blue]%}☁️ %{\$fg[magenta]%}\$sub_name%{\$fg[blue]%} ☁️ %{\$reset_color%}\"
+        fi
+    }"
+    prompt_text="PROMPT+=\" %{\$fg[blue]%}\$(get_current_azure_subscription)\""
+
+    # Function to check and add text to .zshrc
+    function add_to_zshrc {
+        local text="$1"
+        local zshrc="$2"
+        if ! grep -Fq "$text" "$zshrc"; then
+            echo "$text" >> "$zshrc"
+            echo "Added to .zshrc: $text"
+        else
+            echo "Already in .zshrc: $text"
+        fi
+    }
+
+    add_to_zshrc "$function_text" "$ZSHRC"
+    add_to_zshrc "$prompt_text" "$ZSHRC"
+
+echo "\e[1;32mRun 'source $ZSHRC' to apply changes.\e[0m"
+
 }
